96cd9b4137dabcb4076a95df855fdb396f465623
ilya <ilya.ieshin.git@mail.ru>
2025-01-17 22:31:38 +0300

    Initial commit

---
+++ build/Desktop_Qt_6_8_1_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_ServerVoiceApp_FILE C:/Projects/ServerVoiceApp/build/Desktop_Qt_6_8_1_llvm_mingw_64_bit-Debug/ServerVoiceApp.exe)
+set(__QT_DEPLOY_TARGET_ServerVoiceApp_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_ServerVoiceApp_RUNTIME_DLLS C:/MyProgram/Qt/6.8.1/llvm-mingw_64/bin/Qt6Core.dll)
